23d507976093b4d6e7c300ed3c9b257c6fb1766c
Saroj Shrestha <sarojshrestha7253@gmail.com>
2025-06-07 08:55:59 +0545

    even or odd

--- day1.py
+++ day1.py
@@ -1,4 +1,11 @@
-a = 9
-b = 10
-c = a +b
-print("The sum of two number is : ",c)
+# a = 9
+# b = 10
+# c = a +b
+# print("The sum of two number is : ",c)
+
+var = int(input("Enter the any number : "))
+
+if var % 2 == 0 :
+ print("The value is even")
+else: 
+ print("The number is odd")
